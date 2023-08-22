01ab36d59834ca05db84d7df08ecc14d0fc78511
JethroMuluba <jethro.muluba@gmail.com>
2023-08-22 20:32:59 +0000

    creat a price fixer method

--- index.js
+++ index.js
@@ -8,12 +8,21 @@ module.exports = {
 //Create an empty object
 const menu = {
   //Create a _meal property
-  _meal: 1000,
+  _meal: "",
   //Create a _price property
-  _price: "Pizza",
+  _price: 0,
   //Create a meal prep method
   set mealMaker(mealToCheck) {
-
+    if (typeof mealToCheck === 'string') {
+      return this._meal = mealToCheck;
+    }
+  },
+  //create a price fixer method
+  set priceFixer(priceToCheck) {
+    if (typeof priceToCheck === 'number')
+      return this._price = priceToCheck;
   }
+};
 
-}; console.log(menu);
+
+console.log(menu);
